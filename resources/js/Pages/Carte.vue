4945ff4f59ad15c4d7c6839f2a3bf980d46edd53
Odin Wermeille <odin.wermeille@heig-vd.ch>
2024-05-29 15:09:54 +0200

    added routing to map

--- resources/js/Pages/Carte.vue
+++ resources/js/Pages/Carte.vue
@@ -4,51 +4,100 @@
     import leaflet from 'leaflet'
     import { useGeolocation } from '@vueuse/core'
     import { userMarker, nearbyMarkers } from '@/stores/mapStore'
-    import icon from 'leaflet/dist/images/marker-icon.png';
-    import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-    const here = window.location.href
-    const urlArr = here.split(`/`)
+    import 'leaflet/dist/leaflet.css';
+    import 'leaflet-routing-machine';
+    import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+
+
+    const here = window.location.href;
+    const urlArr = here.split(`/`);
     const { coords } = useGeolocation();
-    let map; leaflet.Map;
-    let userGeoMarker; leaflet.Marker
 
-    console.log(leaflet.Icon.Default.prototype._getIconUrl())
-    onMounted(()=>{
-        map = leaflet
-            .map("map")
-            .setView([userMarker.value.latitude, userMarker.value.longitude], 13)
+    let map;
+    let userGeoMarker;
+
+    fetch("/sentiers")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+    });
+
+
+    onMounted(() => {
+    map = leaflet
+        .map("map")
+        .setView([userMarker.value.latitude, userMarker.value.longitude], 13);
+
+    leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    nearbyMarkers.value.forEach(({ latitude, longitude }) => {
         leaflet
-        .tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        })
+        .marker([latitude, longitude])
         .addTo(map)
+        .bindPopup(`Nouveau lieu de l'utilisateur à ${latitude.toFixed(2)}, ${longitude.toFixed(2)}`);
+    });
 
-        map.addEventListener("click", (e)=> {
-            const NewGeoMarker = leaflet.marker([e.latlng.lat, e.latlng.lng])
-            .addTo(map)
-            .bindPopup(`Nouveau lieu de l'utilisateur à ${e.latlng.lat}, ${e.latlng.lng}`);
-        })
-    })
-    
+    map.addEventListener("click", (e) => {
+        const latitude = e.latlng.lat;
+        const longitude = e.latlng.lng;
+        const NewGeoMarker = leaflet.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup(`Nouveau lieu de l'utilisateur à ${latitude.toFixed(2)}, ${longitude.toFixed(2)}`);
+        nearbyMarkers.value.push({ latitude, longitude });
+        console.log(routingControl);
+    });
+
     watchEffect(() => {
         if (coords.value.latitude !== Number.POSITIVE_INFINITY && coords.value.longitude !== Number.POSITIVE_INFINITY) {
-            userMarker.value.latitude = coords.value.latitude+0.014;
-            userMarker.value.longitude = coords.value.longitude+0.014;
+            userMarker.value.latitude = coords.value.latitude + 0.014;
+            userMarker.value.longitude = coords.value.longitude + 0.014;
 
-            
             if (userGeoMarker) {
-                map.removeLayer(userGeoMarker);
+            map.removeLayer(userGeoMarker);
             }
 
             userGeoMarker = leaflet.marker([userMarker.value.latitude, userMarker.value.longitude])
             .addTo(map)
             .bindPopup("Les informations sur le lieu vont ici");
-            map.setView([userMarker.value.latitude, userMarker.value.longitude])
+            map.setView([userMarker.value.latitude, userMarker.value.longitude]);
+
+            const el = userGeoMarker.getElement();
+            if (el) {
+            el.style.filter = "hue-rotate(140deg)";
+            }
         }
+    });
 
+    let routingControl = leaflet.Routing.control({
+        waypoints: [
+            leaflet.latLng([46.783, 6.644]),
+            leaflet.latLng([46.781, 6.646])
+        ],
+        routeWhileDragging: true,
+        show: true,
+        addWaypoints: false,
+        draggableWaypoints: true,
+        router: new leaflet.Routing.OSRMv1({
+            serviceUrl: "http://routing.openstreetmap.de/routed-foot/route/v1"
+        })
+    }).addTo(map);
 
-
+    leaflet.Routing.control({
+        waypoints: [
+            leaflet.latLng([46.783, 6.650]),
+            leaflet.latLng([46.781, 6.651])
+        ],
+        routeWhileDragging: true,
+        show: false,
+        addWaypoints: false,
+        draggableWaypoints: true,
+        router: new leaflet.Routing.OSRMv1({
+            serviceUrl: "http://routing.openstreetmap.de/routed-foot/route/v1"
+        })
+        }).addTo(map);
     });
 </script>
 
@@ -59,6 +108,12 @@
         href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossorigin=""/>
+
+        <link 
+        rel="stylesheet" 
+        href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+
+        
     </Head>
     <p>Hello world {{ urlArr[urlArr.length-1] == "carte" ? "" : urlArr[urlArr.length-1] }}</p>
     <div id="map"></div>
