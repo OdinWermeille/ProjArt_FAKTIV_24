b75749ae42df0f0e3da853a8a42ca0ea87715916
Lohann Kasper <lohann.kasper@me.com>
2024-05-30 09:36:49 +0200

    détail lieu

--- resources/js/Pages/DetailEndroit.vue
+++ resources/js/Pages/DetailEndroit.vue
@@ -16,21 +16,58 @@
                     {{ endroit.description }}
                 </div>
             </div>
-            <img class="map-image" alt="Map" src="map.png" />
+            <div class="input-group map-container">
+                <div id="map" class="rectangle-parent2"></div>
+            </div>
         </div>
     </div>
 </template>
- 
+
+const map = ref(null);
+const latitude = 8.7272;
+const longitude = 5.3627;
+
 <script>
+import { onMounted, ref } from 'vue';
+import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
 export default {
     name: 'DetailEndroit',
     props: {
         endroit: Object,
     },
-    
+    setup(props) {
+        const map = ref(null);
+
+        onMounted(() => {
+            const latitude = props.endroit.coordonneesY;
+            const longitude = props.endroit.coordonneesX;
+
+            console.log(props.endroit);  
+
+            map.value = leaflet
+                .map("map")
+                .setView([latitude, longitude], 13);
+
+            leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            }).addTo(map.value);
+
+            leaflet
+                .marker([latitude, longitude])
+                .addTo(map.value);
+        });
+
+        return {
+            map
+        };
+    }
 };
 </script>
- 
+
+
 <style scoped>
 .container {
     max-width: 600px;
@@ -39,46 +76,45 @@ export default {
     font-family: 'Roboto', sans-serif;
     color: #212121;
 }
- 
- 
+
 .content {
     padding: 20px 0;
 }
- 
+
 .titre {
     font-size: 24px;
     font-weight: bold;
-    height : 48px;
+    height: 48px;
 }
- 
+
 .image-endroit {
     width: 100%;
     height: auto;
     object-fit: cover;
     margin-bottom: 20px;
-    border-radius: 10px; /* Effet arrondi */
+    border-radius: 10px;
 }
- 
+
 .localit-wrapper {
     margin-bottom: 20px;
 }
- 
+
 .localit {
     font-size: 14px;
     color: #666;
 }
- 
+
 .lausanne {
     font-size: 16px;
     font-weight: bold;
 }
- 
+
 .voir-sur-la-carte-wrapper {
     display: flex;
     justify-content: center;
     margin-bottom: 20px;
 }
- 
+
 .voir-sur-la {
     background-color: #4a8c2a;
     color: #fafafa;
@@ -89,29 +125,31 @@ export default {
     border: none;
     cursor: pointer;
 }
- 
+
 .description-wrapper {
     margin-bottom: 20px;
 }
- 
+
 .description {
     font-size: 14px;
     font-weight: bold;
     margin-bottom: 10px;
 }
- 
+
 .la-cathdrale-de {
     font-size: 14px;
     line-height: 1.6;
     color: #666;
 }
- 
-.map-image {
+
+.map-container {
     width: 100%;
-    height: auto;
+    height: 400px; /* Assurez-vous que la hauteur est définie */
     margin-bottom: 20px;
-    border-radius: 10px; /* Effet arrondi pour l'image de la carte aussi */
 }
- 
- 
+
+.rectangle-parent2 {
+    width: 100%;
+    height: 100%; /* Assurez-vous que la hauteur est définie */
+}
 </style>
