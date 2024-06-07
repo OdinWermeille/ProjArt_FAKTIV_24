803e1dd0fc7afe021334b0a5998124eb79c17423
Lohann Kasper <lohann.kasper@me.com>
2024-06-07 10:55:57 +0200

    https leaflet

--- resources/js/Pages/Carte.vue
+++ resources/js/Pages/Carte.vue
@@ -310,7 +310,7 @@ const showSentier = (sentier) => {
             return marker;
         },
         router: new leaflet.Routing.OSRMv1({
-            serviceUrl: "http://routing.openstreetmap.de/routed-foot/route/v1"
+            serviceUrl: "https://routing.openstreetmap.de/routed-foot/route/v1"
         })
     }).addTo(map);
     routingControl.on('routesfound', function () {
@@ -334,7 +334,7 @@ onMounted(() => {
 
     leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
     nearbyMarkers.value.forEach(({ latitude, longitude }) => {
