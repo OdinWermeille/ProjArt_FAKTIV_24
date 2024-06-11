3c6b73e136da38075b8726982fcaaed444683ee0
Micael Ribeiro <micaelribeiro2727@gmail.com>
2024-06-11 15:45:53 +0200

    modif location.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,6 @@
         <sentiers-list></sentiers-list>
     </div>
     <script src="{{ mix('js/app.js') }}"></script>
-
-    <button id="getLocationBtn">Demander la localisation</button>
-
-    <div id="locationInfo" style="display: none;">
-        <p>Latitude: <span id="latitude"></span></p>
-        <p>Longitude: <span id="longitude"></span></p>
-    </div>
-
-    <script src="{{ asset('js/location.js') }}"></script>
 </body>
 
 </html>
