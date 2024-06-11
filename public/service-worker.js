8584654979a182c7cf578f08edfc31ecc0787fcd
Micael Ribeiro <micaelribeiro2727@gmail.com>
2024-06-11 11:10:56 +0200

    modif service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,11 +23,11 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     const fetchCacheFirst = async () => {
-        const cache = await caches.open(CACHE_VERSION);
-        const cached = await cache.match(event.request);
-        if (cached) return cached;
+        //const cache = await caches.open(CACHE_VERSION);
+        //const cached = await cache.match(event.request);
+        //if (cached) return cached;
         let response = await fetch(event.request);
-        if (response) cache.put(event.request, response.clone());
+        //if (response) cache.put(event.request, response.clone());
         return response;
     };
     const url = event.request.url;
